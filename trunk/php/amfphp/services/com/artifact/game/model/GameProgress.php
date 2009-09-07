f13d1ab49b5e58f780ca4ae096a617370e071459
rahulkamra <rahulkamra@2da85ffc-9990-11de-a1b1-8f0f9e247dbb>
2009-09-07 18:01:23 +0000

    public datatype made private

--- trunk/php/amfphp/services/com/artifact/game/model/GameProgress.php
+++ trunk/php/amfphp/services/com/artifact/game/model/GameProgress.php
@@ -14,9 +14,9 @@ class GameProgress {
 
     public $id;
     
-    public $friend;
-    public $csp;
-    public $progressType;
+    private $friend;
+    private $csp;
+    private $progressType;
 
     public function getFriend(){
         return $this->friend;
