52d290dcf805838f6499fb47d9078df889291caf
rahulkamra <rahulkamra@2da85ffc-9990-11de-a1b1-8f0f9e247dbb>
2009-09-10 21:04:56 +0000

    randomizing questions and options  done

--- trunk/php/amfphp/services/com/artifact/game/dao/GameDAO.php
+++ trunk/php/amfphp/services/com/artifact/game/dao/GameDAO.php
@@ -30,5 +30,42 @@ class GameDAO {
         Connection::closeConnection($con);
         return $gameProfile;
     }
+
+    //select * from userprofile where userprofile.userid !=2 order by rand() limit 2
+    //select * from gameprogress where cspid=1 AND friendid =1 AND progresstypeid=(select id from progresstype where progresstype ="SPY")
+    public function getSpyQuestion($user,$friend,$currentSearchParty){
+        $con = Connection::createConnection();
+        //first we need to check weather that person has spied him already or not
+        $spy=ServerConstants::SPY;
+        $friendUser=$friend->user;
+        $chkResult = mysql_query("select * from gameprogress where cspid=$currentSearchParty->id AND friendid =$friendUser->id AND progresstypeid=(select id from progresstype where progresstype ='Spy')");
+        
+        if(mysql_num_rows($chkResult) == 1){
+            Connection::closeConnection($con);
+            return null;
+        }else{
+            $result=mysql_query("select * from userprofile where userprofile.userid !=$friendUser->id order by rand() limit 2");
+            $dataArray=array();
+            while($row = mysql_fetch_array($result)){
+                $userprofile=new UserProfile;
+                $userprofile->id=$row['id'];
+                $userprofile->setUser($user);
+                $userprofile->age=$row['age'];
+                $userprofile->country=$row['country'];
+                $userprofile->favgame=$row['favgame'];
+                $userprofile->humour=$row['humour'];
+                $userprofile->imgurl=$row['imgurl'];
+                $userprofile->job=$row['job'];
+                $userprofile->language=$row['language'];
+                $userprofile->politicalView=$row['politicalview'];
+                $userprofile->religion=$row['religion'];
+                $userprofile->school=$row['school'];
+                array_push($dataArray, $userprofile);
+            }
+        }
+        Connection::closeConnection($con);
+        array_push($dataArray, $friend);//This is the right option
+        return $dataArray;
+    }
 }
 ?>
--- trunk/php/amfphp/services/com/artifact/game/util/GameUtil.php
+++ trunk/php/amfphp/services/com/artifact/game/util/GameUtil.php
@@ -16,5 +16,39 @@ class GameUtil {
         $gameDAO=new GameDAO();
         return $gameDAO->getGameProfile($user);
     }
+
+    public function giveRandomNumbers(){
+        $questionIndex= array();
+        for($count = 0 ;$count < 5 ; $count ++){
+            while(true){
+                $index=rand(2, 10);
+                if(in_array($index, $questionIndex)){
+                    continue;
+                }else{
+                    array_push($questionIndex, $index);
+                    break;
+                }
+            }
+        }
+        return $questionIndex;
+    }
+
+    public function giveUniqueRandomOptions(){
+            $optionIndex= array();
+            for($count = 0 ;$count < 3 ; $count ++){
+                while(true){
+                    $index=rand(0,2);
+                    if(in_array($index, $optionIndex)){
+                        continue;
+                    }else{
+                        array_push($optionIndex, $index);
+                        break;
+                    }
+                }
+            }
+            return $optionIndex;
+    }
+    
+
 }
 ?>
