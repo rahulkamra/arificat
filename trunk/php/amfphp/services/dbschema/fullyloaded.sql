b71863c6f02ad8c3a859c17cc9e4fb0167a54e73
rahulkamra <rahulkamra@2da85ffc-9990-11de-a1b1-8f0f9e247dbb>
2009-09-19 06:50:53 +0000

    version 1.0 done

--- trunk/php/amfphp/services/dbschema/fullyloaded.sql
+++ trunk/php/amfphp/services/dbschema/fullyloaded.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 19, 2009 at 06:37 AM
+-- Generation Time: Sep 19, 2009 at 05:58 AM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.5
 
@@ -58,12 +58,93 @@ CREATE TABLE IF NOT EXISTS `currentsearchparty` (
   PRIMARY KEY  (`id`),
   KEY `FK_currentsearchparty_1` (`userid`),
   KEY `FK_currentsearchparty_2` (`artifactid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=81 ;
 
 --
 -- Dumping data for table `currentsearchparty`
 --
 
+INSERT INTO `currentsearchparty` (`id`, `userid`, `artifactid`, `artifactlvl`, `progress`) VALUES
+(1, 4, 1, 1, 35),
+(2, 2, 1, 1, 0),
+(3, 4, 2, 1, 0),
+(4, 4, 3, 1, 0),
+(5, 4, 4, 1, 0),
+(6, 4, 5, 1, 0),
+(7, 4, 6, 1, 0),
+(8, 4, 7, 1, 0),
+(9, 4, 8, 1, 0),
+(10, 4, 9, 1, 0),
+(11, 4, 10, 1, 0),
+(12, 1, 1, 1, 0),
+(13, 1, 2, 1, 0),
+(14, 1, 3, 1, 0),
+(15, 1, 4, 1, 0),
+(16, 1, 5, 1, 0),
+(17, 1, 6, 1, 0),
+(18, 1, 7, 1, 0),
+(19, 1, 8, 1, 0),
+(20, 1, 9, 1, 0),
+(21, 1, 10, 1, 0),
+(22, 2, 2, 1, 0),
+(23, 2, 3, 1, 0),
+(24, 2, 4, 1, 0),
+(25, 2, 5, 1, 0),
+(26, 2, 6, 1, 0),
+(27, 2, 7, 1, 0),
+(28, 2, 8, 1, 0),
+(29, 2, 9, 1, 0),
+(30, 2, 10, 1, 0),
+(31, 3, 1, 1, 0),
+(32, 3, 2, 1, 0),
+(33, 3, 3, 1, 0),
+(34, 3, 4, 1, 0),
+(35, 3, 5, 1, 0),
+(36, 3, 6, 1, 0),
+(37, 3, 7, 1, 0),
+(38, 3, 8, 1, 0),
+(39, 3, 9, 1, 0),
+(40, 3, 10, 1, 0),
+(41, 5, 1, 1, 0),
+(42, 5, 2, 1, 0),
+(43, 5, 3, 1, 0),
+(44, 5, 4, 1, 0),
+(45, 5, 5, 1, 0),
+(46, 5, 6, 1, 0),
+(47, 5, 7, 1, 0),
+(48, 5, 8, 1, 0),
+(49, 5, 9, 1, 0),
+(50, 5, 10, 1, 0),
+(51, 6, 1, 1, 0),
+(52, 6, 2, 1, 0),
+(53, 7, 1, 1, 0),
+(54, 6, 3, 1, 0),
+(55, 7, 2, 1, 0),
+(56, 6, 4, 1, 0),
+(57, 6, 5, 1, 0),
+(58, 6, 6, 1, 0),
+(59, 7, 3, 1, 0),
+(60, 6, 7, 1, 0),
+(61, 6, 8, 1, 0),
+(62, 7, 4, 1, 0),
+(63, 6, 9, 1, 0),
+(64, 6, 10, 1, 0),
+(65, 7, 5, 1, 0),
+(66, 7, 6, 1, 0),
+(67, 7, 7, 1, 0),
+(68, 7, 8, 1, 0),
+(69, 7, 9, 1, 0),
+(70, 7, 10, 1, 0),
+(71, 8, 1, 1, 0),
+(72, 8, 2, 1, 0),
+(73, 8, 3, 1, 0),
+(74, 8, 4, 1, 0),
+(75, 8, 5, 1, 0),
+(76, 8, 6, 1, 0),
+(77, 8, 7, 1, 0),
+(78, 8, 8, 1, 0),
+(79, 8, 9, 1, 0),
+(80, 8, 10, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -98,6 +179,26 @@ INSERT INTO `experience` (`globallevel`, `exp`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `friendmapping`
+--
+
+CREATE TABLE IF NOT EXISTS `friendmapping` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `userid` int(10) unsigned NOT NULL default '0',
+  `friendid` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FK_friendmapping_1` (`userid`),
+  KEY `FK_friendmapping_2` (`friendid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `friendmapping`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `gameprofile`
 --
 
@@ -113,7 +214,7 @@ CREATE TABLE IF NOT EXISTS `gameprofile` (
   `buylvl` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `FK_gameprofile_1` (`userid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `gameprofile`
@@ -126,7 +227,8 @@ INSERT INTO `gameprofile` (`id`, `userid`, `gold`, `exp`, `globallvl`, `spylvl`,
 (4, 4, 0, 0, 1, 1, 0, 0, 0),
 (5, 5, 0, 0, 1, 1, 0, 0, 0),
 (6, 6, 0, 0, 1, 1, 0, 0, 0),
-(7, 7, 0, 0, 1, 1, 0, 0, 0);
+(7, 7, 0, 0, 1, 1, 0, 0, 0),
+(8, 8, 0, 0, 1, 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -208,7 +310,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `username` varchar(45) NOT NULL default '',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `user`
@@ -221,7 +323,8 @@ INSERT INTO `user` (`id`, `username`) VALUES
 (4, 'Vikas'),
 (5, 'Snaky'),
 (6, 'Kerr'),
-(7, 'Karan');
+(7, 'Karan'),
+(8, 'Aman');
 
 -- --------------------------------------------------------
 
@@ -244,7 +347,7 @@ CREATE TABLE IF NOT EXISTS `userprofile` (
   `imgurl` varchar(200) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `FK_userprofile_1` (`userid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 4096 kB; (`id`) REFER `artifactdb/user`(`id`)' AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 4096 kB; (`id`) REFER `artifactdb/user`(`id`)' AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `userprofile`
@@ -257,7 +360,8 @@ INSERT INTO `userprofile` (`id`, `userid`, `age`, `politicalview`, `religion`, `
 (4, 4, 22, 'Communist', 'Agnostic', 'Japanese', 'intelligent', 'Japan', 'Tokyo University', 'Game Developer', 'Defence of the Ancients', 'com/artifact/resources/images/avatar/cathelmet.jpg'),
 (5, 5, 27, 'Republican', 'Buddhism', 'English', 'polite', 'India', 'BHU', 'Politician', 'Halo', 'com/artifact/resources/images/avatar/ba-na-na.gif'),
 (6, 6, 18, 'Liberal', 'Christianity', 'English', 'Cheesy', 'Australia', 'Melbourne High', 'None', 'Counter Strike', 'com/artifact/resources/images/avatar/disappointed000.gif'),
-(7, 7, 29, 'Democratic', 'Christianity', 'English', 'Campy', 'Canada', 'Toronto University', 'Manager', 'Diablo 2', 'com/artifact/resources/images/avatar/yoda-dance.gif');
+(7, 7, 29, 'Democratic', 'Christianity', 'English', 'Campy', 'Canada', 'Toronto University', 'Manager', 'Diablo 2', 'com/artifact/resources/images/avatar/yoda-dance.gif'),
+(8, 8, 24, 'Socialist', 'Hindu', 'French', 'Goofy', 'Spain', 'DPS', 'C.A.', 'GTA ', 'com/artifact/resources/images/avatar/uchiha_itachi.jpg');
 
 --
 -- Constraints for dumped tables
@@ -269,6 +373,13 @@ INSERT INTO `userprofile` (`id`, `userid`, `age`, `politicalview`, `religion`, `
 ALTER TABLE `currentsearchparty`
   ADD CONSTRAINT `FK_currentsearchparty_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `FK_currentsearchparty_2` FOREIGN KEY (`artifactid`) REFERENCES `artifactinfo` (`id`);
+
+--
+-- Constraints for table `friendmapping`
+--
+ALTER TABLE `friendmapping`
+  ADD CONSTRAINT `FK_friendmapping_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_friendmapping_2` FOREIGN KEY (`friendid`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `gameprofile`
